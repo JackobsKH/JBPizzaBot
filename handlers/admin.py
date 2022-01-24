058ff5c60f84099c4b608236fd008bdd2fb2e7e6
JackobsKH <jbkherson@gmail.com>
2022-01-24 12:43:19 +0200

    Update admin.py
    need

--- handlers/admin.py
+++ handlers/admin.py
@@ -114,6 +114,5 @@ def register_handlers_admin(dp : Dispatcher):
 	dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
 	dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
 	dp.register_message_handler(delete_item, commands=['Удалить'], state=None)
-	# dp.register_message_handler(del_callback_run, lambda x: x.data and x.data.startswith('del '))
 	dp.register_callback_query_handler(del_callback_run, lambda x: x.data and x.data.startswith('del '))
 
